f9e9e2cfc93d52efb3b8e8530c95899762c3a7a9
Rajkishor shukla <rajkishorshukla867@gmail.com>
2025-08-23 19:52:42 +0530

    removed footer import

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import HeroSlider from './HeroSlider.jsx'
-import TestimonialSlider from './TestimonialSlider.jsx'
-import AboutUs from './AboutUs.jsx'
-import Footer from './footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    {/* <HeroSlider/>
-    <TestimonialSlider/>
-    <AboutUs/>
-    <Footer/> */}
   </StrictMode>,
 )
